f8005596de6634c509556736d2c15ddc0000c911
doobiwan <craig@dubiant.net>
2012-06-23 15:32:11 +1000

    Added Models

---
+++ SequeBeat/Model/Measure.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequeBeat.Model
+{
+    class Measure
+    {
+        public double Gain { get; set; }
+        public double PushPull { get; set; }
+        public Sample Sample { get; set; }
+        public IList<Note> Notes { get; set; }
+    }
+}
---
+++ SequeBeat/Model/Sequence.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SequeBeat.Model
+{
+    class Sequence
+    {
+        public string Name { get; set; }
+        public string Notes { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime Modified { get; set; }
+        public int Bpm { get; set; }
+        public IList<Measure> Measures { get; set; }
+    }
+}
